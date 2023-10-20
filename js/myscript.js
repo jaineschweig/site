1871e07cfffd1df0c1a52d172592b30e2f4bb731
jaineschweig <142935669+jaineschweig@users.noreply.github.com>
2023-10-20 17:50:19 +0000

    sim

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function mudarFrase() {
+    
+    elemento.innerHTML = "Qualquer frasvar elemento = document.getElementById("frase")e"
+    elemento.style.color = "red";
+    elemento.style.fontSize = "25px";
+    elemento.style.backgroundColor = "yellow";
+    document.body.style.backgroundColor = "blue";
+    elemento.innerHTML = "<img src=\'img/a.jpeg\' />"
+}
